2738b73d8baa9921011987251dc1515bbff1b312
Idrise Abdi <Idrise.t.Abdi@nasa.gov>
2023-06-20 15:50:16 -0500

    sp

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ def binarySearch(lst, key):
   # Initialize two pointers, 'low' and 'high', to the start and end of the list respectively
   low = 0
   high = len(lst) - 1
+  
+  #initialize a counter to count the number of iterations
+  counter = 0
 
   # Start a loop that continues as long as 'low' is less than or equal to 'high'
   while high >= low:
+    
+    #increment the counter
+    counter += 1
+    
     # Calculate the middle index by taking the floor division of the sum of 'low' and 'high'
     mid = (low + high) // 2
 
@@ -21,7 +28,9 @@ def binarySearch(lst, key):
     # If the search key is equal to the middle element, it means we've found the key.
     # So, return the 'mid' index
     elif key == lst[mid]:
-      return mid
+      #return mid and the counter value
+      return mid, counter
+      # return mid
 
     # If the search key is greater than the middle element,
     # it means the key if present, would be in the second half of the list.
@@ -32,16 +41,20 @@ def binarySearch(lst, key):
   # If the search key is not in the list, return a negative index which is a customary way
   # to indicate that the item is not in the list. '-low - 1' is chosen so that if you were
   # to negate the return value and subtract 1, you'd get the index where the key should be inserted.
-  return -low - 1
+  # return -low and the counter value
+  return -low, counter
+  # return -low - 1
 
 
-timedata = open('timedataforbinarysearch.csv', 'w')
+timedata = open('timedataforbinarysearch2.csv', 'w')
 max_number_tosearch_and_seed = 1000001 # 1 million
 num_runs = 10  # Number of times to run binarySearch for each list size
 
-for n in range(10000, 1000001, 10000):
+for n in range(1000, 100001, 1000):
     total_time = 0
-
+    #initialize totalCounter to 0
+    totalCounter = 0
+    
     for _ in range(num_runs):
         lst = [random.randint(1, max_number_tosearch_and_seed) for _ in range(n)]
         lst.sort()
@@ -50,17 +63,24 @@ for n in range(10000, 1000001, 10000):
         key = lst[keyIndex]
 
         t0 = time.perf_counter()
-        binarySearch(lst, key)
+        #call binarySearch function and store the return value and counter value in two variables
+        index, counter = binarySearch(lst, key)
         t1 = time.perf_counter()
 
         total_time += (t1 - t0)
+        totalCounter += counter
 
     average_time = (total_time / num_runs) * 1000.0
-    total_time_str = "{:.12f}".format(average_time)
-
-    timedata.write(str(n) + ',' + total_time_str + '\n')
+    total_time_avg_str = "{:.12f}".format(average_time)
+    
+    #get average counter value
+    average_counter = totalCounter / num_runs
+    average_counter_str = "{:.2f}".format(average_counter)
+    
+    timedata.write(str(n) + ',' + total_time_avg_str + ',' + average_counter_str + '\n')
     print('List Size:', n)
-    print('Average Time Taken:', total_time_str)
+    print('Average Time Taken:', total_time_avg_str)
+    print('Average Search Loops:', average_counter)
     print('---')
 
 timedata.close()
